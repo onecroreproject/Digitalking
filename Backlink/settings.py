3f86918ed8d2cea292cc2f515ca4523464eb2bec
Gowtham123-S <gowtham.13ni@gmail.com>
2025-08-06 15:40:52 +0530

    changes to be done

--- Backlink/settings.py
+++ Backlink/settings.py
@@ -81,15 +81,15 @@ LOGIN_REDIRECT_URL = '/move-temp/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": config('DB_NAME'),
-        "USER": config('DB_USER'),
-        "PASSWORD": config('DB_PASSWORD'),
-        "HOST": config('DB_HOST'),
-        "PORT": config('DB_PORT')
-    }
-    # 'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+    # "default": {
+    #     "ENGINE": "django.db.backends.postgresql",
+    #     "NAME": config('DB_NAME'),
+    #     "USER": config('DB_USER'),
+    #     "PASSWORD": config('DB_PASSWORD'),
+    #     "HOST": config('DB_HOST'),
+    #     "PORT": config('DB_PORT')
+    # }
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
 
 
